1663153ce75bb10c5063fdd994451cc77726565c
Gabriel Paulino <gabrielpaulino06@hotmail.com>
2023-07-04 08:16:38 -0300

    User entity and resource

---
+++ src/main/java/com/myprojects/course/entities/User.java
@@ -0,0 +1,84 @@
+package com.myprojects.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String naem;
+	private String email;
+	private String phone;
+	private String password;
+	
+	public User() {
+	}
+
+	public User(Long id, String naem, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.naem = naem;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNaem() {
+		return naem;
+	}
+
+	public void setNaem(String naem) {
+		this.naem = naem;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+}
